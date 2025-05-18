cf018c0f3f3520ce1136d1a3019c6b30456651db
Himesh  Vats <himeshvats@Himeshs-Mac-mini.local>
2025-05-18 21:28:12 +0530

    Added Podcast initial codebase

---
+++ src/app/components/AudioPlayer.tsx
@@ -0,0 +1,168 @@
+"use client";
+
+import React, { useState, useEffect, useRef } from "react";
+import AudioControls from "./AudioControls";
+import Backdrop from "./Backdrop";
+
+interface Track {
+    title: string;
+    artist: string;
+    color: string;
+    image: string;
+    audioSrc: string;
+}
+
+interface AudioPlayerProps {
+    tracks: Track[];
+}
+
+const AudioPlayer: React.FC<AudioPlayerProps> = ({ tracks, currentTrack, whatsPlayingCallback }) => {
+
+    useEffect(() => {
+        if (currentTrack) {
+            setTrackIndex(currentTrack - 1);
+        }
+    }, [currentTrack]);
+
+
+
+    const [trackIndex, setTrackIndex] = useState<number>(0);
+    const [trackProgress, setTrackProgress] = useState<number>(0);
+    const [isPlaying, setIsPlaying] = useState<boolean>(false);
+
+    const { title, artist, color, image, audioSrc } = tracks[trackIndex];
+
+    useEffect(() => {
+        if (whatsPlayingCallback) {
+            whatsPlayingCallback(trackIndex + 1);
+        }
+    }, [trackIndex]);
+
+    const audioRef = useRef<HTMLAudioElement>(new Audio(audioSrc));
+    const intervalRef = useRef<NodeJS.Timeout | null>(null);
+    const isReady = useRef<boolean>(false);
+
+    const { duration } = audioRef.current;
+
+    const currentPercentage = duration
+        ? `${(trackProgress / duration) * 100}%`
+        : "0%";
+    const trackStyling = `
+    -webkit-gradient(linear, 0% 0%, 100% 0%, color-stop(${currentPercentage}, #fff), color-stop(${currentPercentage}, #777))
+  `;
+
+    const startTimer = (): void => {
+        if (intervalRef.current) {
+            clearInterval(intervalRef.current);
+        }
+
+        intervalRef.current = setInterval(() => {
+            if (audioRef.current.ended) {
+                toNextTrack();
+            } else {
+                setTrackProgress(audioRef.current.currentTime);
+            }
+        }, 1000);
+    };
+
+    const onScrub = (value: string): void => {
+        if (intervalRef.current) {
+            clearInterval(intervalRef.current);
+        }
+        audioRef.current.currentTime = parseFloat(value);
+        setTrackProgress(audioRef.current.currentTime);
+    };
+
+    const onScrubEnd = (): void => {
+        if (!isPlaying) {
+            setIsPlaying(true);
+        }
+        startTimer();
+    };
+
+    const toPrevTrack = (): void => {
+        if (trackIndex - 1 < 0) {
+            setTrackIndex(tracks.length - 1);
+        } else {
+            setTrackIndex(trackIndex - 1);
+        }
+    };
+
+    const toNextTrack = (): void => {
+        if (trackIndex < tracks.length - 1) {
+            setTrackIndex(trackIndex + 1);
+        } else {
+            setTrackIndex(0);
+        }
+    };
+
+    useEffect(() => {
+        if (isPlaying) {
+            audioRef.current.play();
+            startTimer();
+        } else {
+            audioRef.current.pause();
+        }
+    }, [isPlaying]);
+
+    useEffect(() => {
+        audioRef.current.pause();
+        audioRef.current = new Audio(audioSrc);
+        setTrackProgress(audioRef.current.currentTime);
+
+        if (isReady.current) {
+            audioRef.current.play();
+            setIsPlaying(true);
+            startTimer();
+        } else {
+            isReady.current = true;
+        }
+    }, [trackIndex, audioSrc]);
+
+    useEffect(() => {
+        return () => {
+            audioRef.current.pause();
+            if (intervalRef.current) {
+                clearInterval(intervalRef.current);
+            }
+        };
+    }, []);
+
+    return (
+        <div className="audio-player">
+            <div className="track-info">
+                <img
+                    className="artwork"
+                    src={image}
+                    alt={`track artwork for ${title} by ${artist}`}
+                />
+                <h2 className="title">{title}</h2>
+                <AudioControls
+                    isPlaying={isPlaying}
+                    onPrevClick={toPrevTrack}
+                    onNextClick={toNextTrack}
+                    onPlayPauseClick={setIsPlaying}
+                />
+                <input
+                    type="range"
+                    value={trackProgress}
+                    step="1"
+                    min="0"
+                    max={duration ? duration : 0}
+                    className="progress"
+                    onChange={(e) => onScrub(e.target.value)}
+                    onMouseUp={onScrubEnd}
+                    onKeyUp={onScrubEnd}
+                    style={{ background: trackStyling }}
+                />
+            </div>
+            <Backdrop
+                trackIndex={trackIndex}
+                activeColor={color}
+                isPlaying={isPlaying}
+            />
+        </div>
+    );
+};
+
+export default AudioPlayer;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,88 @@
-import Image from "next/image";
+"use client";
+import React, { useEffect, useState, useRef } from "react";
+import ExpandableCardDemo from './components/Listing';
+import AudioPlayer from './components/AudioPlayer';
+import WaveSurfer from 'wavesurfer.js'
+
+
+
+
+const tracks: any = [
+  {
+    id: 1,
+    title: "Radio 1's Drag Queen's Den",
+    artist: "",
+    audioSrc: './audio/Radio1sDragQueensDen.mp3',
+    image: 'https://ichef.bbci.co.uk/images/ic/640x360/p06xt8x3.jpg',
+    color: "#00aeb0"
+  },
+  {
+    id: 2,
+    title: "The Untold",
+    artist: "",
+    audioSrc: './audio/TheUntold.mp3',
+    image: 'https://ichef.bbci.co.uk/images/ic/640x360/p06w4g8c.jpg',
+    color: "#ffb77a"
+  },
+  {
+    id: 3,
+    title: "How To Invent A Country",
+    artist: "",
+    audioSrc: './audio/HowToInventACountry.mp3',
+    image: 'https://ichef.bbci.co.uk/images/ic/640x360/p0683mt5.jpg',
+    color: "#5f9fff"
+  },
+  {
+    id: 4,
+    title: "Live Lounge Uncovered",
+    artist: "",
+    audioSrc: './audio/LiveLoungeUncovered.mp3',
+    image: 'https://ichef.bbci.co.uk/images/ic/640x360/p06rvmm2.jpg',
+    color: "#5f9fff"
+  },
+  {
+    id: 5,
+    title: "The Case Of Charles Dexter Ward",
+    artist: "",
+    audioSrc: './audio/TheCaseOfCharlesDexterWard.mp3',
+    image: 'https://ichef.bbci.co.uk/images/ic/640x360/p06tm8tb.jpg',
+    color: "#5f9fff"
+  }
+]
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  const [currentTrack, setCurrentTrackCallBack] = useState<number>(0);
+  const [activeTrack, setActiveTrack] = useState<number>(0);
+
+  useEffect(() => {
+    const wavesurfer = WaveSurfer.create({
+      container: document.getElementById('waveform'),
+      waveColor: 'grey',
+      progressColor: 'rgb(100, 0, 100)',
+      interact: false,
+      url: tracks[currentTrack].audioSrc,
+
+      // Set a bar width
+      barWidth: 5,
+      // Optionally, specify the spacing between bars
+      barGap: 2,
+      // And the bar radius
+      barRadius: 2,
+    })
+  }, [])
+
+  return <>
+    <div style={{ display: 'flex', justifyContent: 'space-around', padding: 100, zIndex: 10, height: '85vh' }}>
+      <div className="w-3/5" style={{ zIndex: 1 }}>
+        <h1>Top 5 Podcasts of the Week!</h1>
+        <ExpandableCardDemo currentTrackCallback={(id: number) => setCurrentTrackCallBack(id)} activeTrack={activeTrack} />
+      </div>
+      <div className="w-2/5" >
+        <AudioPlayer tracks={tracks} currentTrack={currentTrack} whatsPlayingCallback={(trackIndex) => setActiveTrack(trackIndex)} />
+      </div>
     </div>
-  );
+    <div id="waveform" style={{ width: '100%', height: 128 }}></div>
+  </>
 }
+
